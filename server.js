69a624a057d54c69f672a5b5f5f33620c443b9c6
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-14 15:36:56 +0100

    initial comit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const port=3000;
+
+const server = express();
+
+server.set("view engine", "hbs");
+
+server.get ('/', (request, response) => {
+    response.render("main");
+})
+
+server.listen(port);
