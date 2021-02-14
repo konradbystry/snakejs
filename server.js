f721634d95faa6215c05027f6bd587940910b91f
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-14 21:23:53 +0100

    fixed directories

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const port=3000;
+const port = 3000;
 const app = express();
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname +"/public"));
 
-app.set("view engine", "hbs");
-app.get ('/', (request, response) => {
-    response.render("index.hbs");
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + "/index.html");
 })
 
 app.listen(port);
