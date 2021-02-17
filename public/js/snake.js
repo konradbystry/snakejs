80eb7f69882af958ea5a90a5f927000912147514
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-17 18:08:09 +0100

    created move function

--- public/js/snake.js
+++ public/js/snake.js
@@ -1,7 +1,7 @@
 export default class Snake{
     
     constructor(){
-        this.snake = [{x : 280, y : 300}]; 
+        this.snake_body = [{x : 280, y : 300}, {x : 300, y : 300}, {x : 320, y :300}]; 
         this.ctx = document.querySelector("#game_canvas").getContext("2d");  
     }
     
@@ -13,9 +13,21 @@ export default class Snake{
    */
     
    draw_snake(snake_part){
-        this.snake.forEach((snake_part) => {
+        this.snake_body.forEach((snake_part) => {
             this.ctx.fillRect(snake_part.x, snake_part.y, 20, 20);
         });
+    }
+
+    eat(snake_part){
+       
+        if(this.snake_body[0].x);
+    }
+
+    move(direction_x, direction_y){
+        let snake_head = this.snake_body[0]; 
+        console.log(snake_head);
+        this.snake_body.pop();
+        this.snake_body.unshift({ x : snake_head.x + direction_x , y: snake_head.y + direction_y })
     }
 
 }
