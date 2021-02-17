102ff06a054ab560efe56f8ba56660b97d5d6019
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-17 14:58:23 +0100

    fixed draw_snake function

--- public/js/snake.js
+++ public/js/snake.js
@@ -1,19 +1,18 @@
 export default class Snake{
     
-    constructor(ctx){
-        this.snake=[{x : 280, y : 300}];   
+    constructor(){
+        this.snake = [{x : 280, y : 300}]; 
+        this.ctx = document.querySelector("#game_canvas").getContext("2d");  
     }
     
-    draw_snake_part(snake_part) {
-        ctx.fillRect(snake_part.x, snake_part.y, 20, 20);
+    draw_snake_part(snake_part){
+       
+        this.ctx.fillRect(snake_part.x, snake_part.y, 20, 20);
     }
 
-    draw_snake(snake_part) {
-        this.snake.forEach(draw_snake_part) ;
+    draw_snake(snake_part){
+        this.snake.forEach(this.draw_snake_part(snake_part));
     }
 
-    score_point(snake) {
-        this.snake.push()  //todo
-    }
 }
 
