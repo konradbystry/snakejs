9f0ae27646b2af272546c9240616a8ffa8191b6a
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-18 14:00:23 +0100

    used new functions for snake moving directions

--- public/js/game.js
+++ public/js/game.js
@@ -1,14 +1,8 @@
 import Snake from "./snake.js"
+import Game_board from "./game_board.js"
 
-const game_board = document.querySelector("#game_canvas");
-
-let ctx = game_board.getContext("2d");
-
-function clear_canvas() {
-    ctx.clearRect(0, 0, game_board.width, game_board.height);
-}
-
-const snake=new Snake();
+const game_board = new Game_board();
+const snake = new Snake();
 let snake_body = snake.snake_body;
 
 snake.draw_snake(snake_body);
@@ -17,24 +11,28 @@ window.addEventListener("keydown", (event)=>{
     event.preventDefault();
     
     if(event.key == "ArrowLeft"){
-        snake.direction_x=-20;
-        snake.direction_y=0;
-    }
+        snake.set_direction_x(snake.move_left.x);
+        snake.set_direction_y(snake.move_left.y);
+    }  
     if(event.key == "ArrowRight"){
-        snake.direction_x=20;
-        snake.direction_y=0;
+        snake.set_direction_x(snake.move_right.x);
+        snake.set_direction_y(snake.move_right.y);
     }  
     if(event.key == "ArrowUp"){
-        snake.direction_x=0;
-        snake.direction_y=-20;
+        snake.set_direction_x(snake.move_up.x);
+        snake.set_direction_y(snake.move_up.y);
     }  
     if(event.key == "ArrowDown"){
-        snake.direction_x=0;
-        snake.direction_y=20;
+        snake.set_direction_x(snake.move_down.x);
+        snake.set_direction_y(snake.move_down.y);
     }          
 })
 
-setInterval(function onTick(){console.log(snake.direction_x, snake.direction_y);clear_canvas(); snake.move(snake.direction_x, snake.direction_y); snake.draw_snake(snake_body)}, 100);
+setInterval(function onTick(){
+    console.log(snake.direction_x, snake.direction_y); 
+    game_board.clear_game_board(); 
+    snake.move(); 
+    snake.draw_snake(snake_body)}, 100);
 
     
 
